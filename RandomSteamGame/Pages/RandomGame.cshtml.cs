2027e66a04b4f25187a34c820008be4ea4fa1bb5
Kyle Givler <xvegan88x@gmail.com>
2022-12-19 21:05:23 -0500

    Need to clean this up, but prevent an infinite loop here...

--- RandomSteamGame/Pages/RandomGame.cshtml.cs
+++ RandomSteamGame/Pages/RandomGame.cshtml.cs
@@ -11,16 +11,22 @@ namespace RandomSteamGame.Pages
         [BindProperty]
         public Int64 SteamId { get; set; } = default!;
 
-        public AppDetailsResponse AppDetails { get; set; }
-        public Game Game { get; set; }
+        public AppDetailsResponse AppDetails { get; set; } = new();
+        public Game Game { get; set; } = default!;
+        public string? ErrorMessage { get; set; } = null;
 
         private readonly SteamService _steamService;
         private readonly SteamStoreService _steamStoreService;
+        private readonly ILogger<RandomGameModel> _logger;
 
-        public RandomGameModel(SteamService steamService, SteamStoreService steamStoreService)
+        public RandomGameModel(
+            SteamService steamService, 
+            SteamStoreService steamStoreService,
+            ILogger<RandomGameModel> logger)
         {
             _steamService = steamService;
             _steamStoreService = steamStoreService;
+            _logger = logger;
         }
 
         public void OnGet()
@@ -32,8 +38,23 @@ namespace RandomSteamGame.Pages
             //76561197988408972
             var gamesOwned = await _steamService.GetOwnedGames(SteamId);
 
-            Game = gamesOwned.Games[Random.Shared.Next(0, gamesOwned.GameCount -1)];
-            AppDetails = await _steamStoreService.GetAppData(Game.AppId);
+            int attempts = 0;
+            while(!AppDetails.Success)
+            {
+                Game = gamesOwned.Games[Random.Shared.Next(0, gamesOwned.GameCount - 1)];
+                AppDetails = await _steamStoreService.GetAppData(Game.AppId);
+
+                if(!AppDetails.Success)
+                {
+                    // TODO: Clean this up. We don't want an infinte loop if no app data can be retreived
+                    attempts++;
+                    if(attempts >= 3)
+                    {
+                        throw new Exception("Unable to get app details");
+                    }
+                    _logger.LogWarning("Unable to get app details for {AppId}", Game.AppId);
+                }
+            }
         }
     }
 }
