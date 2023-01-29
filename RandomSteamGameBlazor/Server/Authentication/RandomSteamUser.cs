e579d0433b864df2aec08e2734718cbe6e930899
Kyle Givler <xvegan88x@gmail.com>
2023-01-29 18:50:55 -0500

    Can Register and login

--- RandomSteamGameBlazor/Server/Authentication/RandomSteamUser.cs
+++ RandomSteamGameBlazor/Server/Authentication/RandomSteamUser.cs
@@ -8,6 +8,8 @@ public class RandomSteamUser : IdentityUser
 {
     public string FirstName { get; set; } = default!;
     public string LastName { get; set; } = default!;
+    public string? VanityUrl { get; set; }
+    public long? SteamId { get; set; }
     public string? RefreshToken { get; set; }
     public DateTime RefreshTokenExpiryTime { get; set; }
 }
