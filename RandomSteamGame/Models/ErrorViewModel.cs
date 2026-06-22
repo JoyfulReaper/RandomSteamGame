37ff24c717decbbd69835bfd8c12048849a1d029
Kyle Givler <xvegan88x@gmail.com>
2026-06-22 19:17:46 -0400

    Remove POC MVC project

--- RandomSteamGame/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace RandomSteamGame.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- RandomSteamGame/Models/RandomGameViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace RandomSteamGame.Models;
-
-public class RandomGameViewModel
-{
-    public long SteamId { get; set; }
-    public string? CustomUrl { get; set; }
-}
