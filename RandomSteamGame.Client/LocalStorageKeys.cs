69cae0c602a8f0a2f5d038eba27ab2519d71a67d
Kyle Givler <xvegan88x@gmail.com>
2026-06-28 21:48:39 -0400

    Remove token authentication storage
    Remove RefreshToken and Token constants from LocalStorageKeys and delete the Logout method that depended on them. This authentication token management is no longer used by the application.

--- RandomSteamGame.Client/LocalStorageKeys.cs
+++ RandomSteamGame.Client/LocalStorageKeys.cs
@@ -9,8 +9,6 @@ namespace RandomSteamGame.Client;
 
 public static class LocalStorageKeys
 {
-    public const string RefreshToken = "refreshToken";
-    public const string Token = "token";
     public const string SteamId = "steamId";
     public const string VanityUrl = "vanityUrl";
 }
